b4f533c9266e234097b89a817cf9b67045b3a651
Hephzibah777 <Hephzibahr@mindfiresolutions.com>
2025-01-15 21:12:13 +0530

    updated changes on jquery till edit

--- script2.js
+++ script2.js
@@ -1,7 +1,19 @@
-(document).ready(function () {
-    
+$(document).ready(function () {
+    let i=0;
+    let editrow=null;
     $("#myForm").on("submit", function (e) {
         e.preventDefault();
+        if(editrow!=null){
+            let temp=$("#myTable tbody");
+            console.log(temp);
+        //    $(`#myTable tbody tr:nth-child(${editrow.id}) tr:nth-child(0)`).val(editrow.email);
+        //    $(`#myTable tbody tr:nth-child(${editrow.id}) tr:nth-child(1)`).val(editrow.phone);
+        //    $(`#myTable tbody tr:nth-child(${editrow.id}) tr:nth-child(2)`).val(editrow.firstname);
+        //    $(`#myTable tbody tr:nth-child(${editrow.id}) tr:nth-child(3)`).val(editrow.lastname);
+            $("#buttondetail").text("Submit Application");
+            editrow=null;
+        }
+        else{
         var formData = {
             email: $("#email").val(),
             password: $("#password").val(),
@@ -17,7 +29,8 @@
             phone: $("#phone").val(),
             date: $("#date").val(),
         }
-
+        formData.id=i;
+        i=i+1;
         if ($('#yes').is(':checked')) {
             formData.relocation = "yes";
         }
@@ -27,7 +40,8 @@
         else {
             formData.relocation = "";
         }
-        console.log(formData);
+
+
 
         var validation = {
             email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
@@ -47,47 +61,57 @@
 
 
         }
+
+
         $.fn.validatefunction = function (key, value) {
             if (value && validation[key].test(value)) return true;
             else return false;
 
         }
+
+        var isValidfields=true;
         $.each(formData, function (key, value) {
-            var isValid = $.fn.validatefunction(key, value);
-            if (isValid == false) {
+             var isValid = $.fn.validatefunction(key, value);
+            if (isValid == false && key!="id") {
+                isValidfields=false;
+                console.log(`${key}` + isValidfields);
                 var keystr = String(key) + "-error";
-                if (key == "email") {
+                console.log(`${key}`);
+                if (key === "email") {
                     $("#" + keystr).text("Input is invalid");
+                }   
+                else
+                if (key == "password") {
+                    $("#" + keystr).text("Password should contain atleast one digit, one uppercase, one lowercase.");
                 }
                 else
-                    if (key == "password") {
-                        $("#" + keystr).text("Password should contain atleast one digit, one uppercase, one lowercase.");
-                    }
-                    else
-                    if (key == "phone") {
-                        $("#" + keystr).text("Phone should consists of 10 digits");
-                        }
-                    else
-                    if(key == "pin") {
-                        $("#" + keystr).text("Pin should not be null and should contain only numbers");
-                        }
-                    else{
-                        $("#" + keystr).text("Input is Missing");
-                        }
-
-
-            }
-            else {
-                console.log("it is valid");
+                if (key == "phone") {
+                    $("#" + keystr).text("Phone should consists of 10 digits");
+                }
+                else {
+                    $("#" + keystr).text("Input is Missing");
+                }
             }
         })
-
+        
+        if(isValidfields==true){
+            $.fn.createtoastfunction("Submitted Successfully");
+            $.fn.addrows(formData);
+        }
+        else{
+            $.fn.createtoastfunction("Invalid Credentials");
+        }
+        $("#myForm")[0].reset();
+    }
     });
+    
 
 
     $("input").focus(function () {
         $(this).css("backgroundColor", "grey");
     });
+
+
     $("input").blur(function () {
         $(this).css("backgroundColor", "");
     });
@@ -98,7 +122,7 @@
             $(this).css("borderColor", "red");
             $("#email-error").text("Please Enter a Valid Email Address");
         }
-        else{
+        else {
             $(this).css("borderColor", "green");
             $("#email-error").text("");
         }
@@ -110,64 +134,197 @@
             $(this).css("borderColor", "red");
             $("#phone-error").text("Phone Number should be of exactly 10 Digits");
         }
-        else{
+        else {
             $(this).css("borderColor", "green");
             $("#phone-error").text("");
         }
     })
 
-//password validation
-var myInput = $("#password").val();
+    //password validation
+    $("#password").focus(function () {
+        $("#message").css("display", "block");
+    })
 
 
-$("#password").focus(function(){
-    $("#message").css("display", "block");
-})
-$("#password").blur(function(){
-    $("#message").css("display", "none");
-})
+    $("#password").blur(function () {
+        $("#message").css("display", "none");
+    })
 
-$("#password").keyup(function(){
-    // Validate lowercase letters
-    var lowerCaseLetters = /[a-z]/g;
-    if (myInput.match(lowerCaseLetters)) {
-        $("#letter").removeClass("invalid");
-        $("#letter").addClass("valid");
-    } else {
-        $("#letter").removeClass("valid");
-        $("#letter").addClass("invalid");
+    $("#password").keyup(function () {
+
+        // Validate lowercase letters
+        var lowerCaseLetters = /[a-z]/g;
+        if ($("#password").val().match(lowerCaseLetters)) {
+            $("#letter").removeClass("invalid");
+            $("#letter").addClass("valid");
+        } else {
+            $("#letter").removeClass("valid");
+            $("#letter").addClass("invalid");
+        }
+
+        // Validate capital letters
+        var upperCaseLetters = /[A-Z]/g;
+        if ($("#password").val().match(upperCaseLetters)) {
+            $("#capital").removeClass("invalid");
+            $("#capital").addClass("valid");
+        } else {
+            $("#capital").removeClass("valid");
+            $("#capital").addClass("invalid");
+        }
+
+        // Validate numbers
+        var numbers = /[0-9]/g;
+        if ($("#password").val().match(numbers)) {
+            $("#number").removeClass("invalid");
+            $("#number").addClass("valid");
+        } else {
+            $("#number").removeClass("valid");
+            $("#number").addClass("invalid");
+        }
+
+        // Validate length
+        if ($("#password").val().length >= 8) {
+            $("#length").removeClass("invalid");
+            $("#length").addClass("valid");
+        } else {
+            $("#length").removeClass("valid");
+            $("#length").addClass("invalid");
+        }
+
+    })
+
+    const data = {
+        "USA": {
+            "New York": ["Manhattan", "Brooklyn", "Queens"],
+            "California": ["Los Angeles", "San Francisco", "San Diego"]
+        },
+        "India": {
+            "Maharashtra": ["Mumbai", "Pune", "Nagpur"],
+            "Delhi": ["South Delhi", "North Delhi", "West Delhi"]
+        }
+    };
+
+
+    $.each(data, function (key) {
+        let option = $("<option></option");
+        option.val(key);
+        option.text(key);
+        $("#country").append(option);
+
+    })
+
+    $("#country").change(function () {
+        const countryselected = $("#country").val();
+        if (countryselected) {
+            $.each(data[countryselected], function (key) {
+                let option = $("<option></option");
+                option.val(key);
+                option.text(key);
+                $("#city").append(option);
+            })
+        }
+    })
+    $("#city").change(function () {
+        const countryselected = $("#country").val();
+        const cityselected = $("#city").val();
+        if (countryselected && cityselected) {
+            $.each(data[countryselected][cityselected], function (key, value) {
+                let option = $("<option></option");
+                option.val(value);
+                option.text(value);
+                $("#area").append(option);
+            })
+        }
+    })
+
+
+    $.fn.createtoastfunction = function (msg) {
+       let toast = $("<div></div>");
+       let toastBox = $("#toastBox");
+       toastBox.addClass("toast");
+       toast.html(msg);
+       toastBox.append(toast);
+
+      if(msg.includes('Invalid')){
+        toastBox.addClass("invalid");
+      }
+      
+      setTimeout(() => {
+                toastBox.remove();
+            }, 6000);
+        
     }
 
-    // Validate capital letters
-    var upperCaseLetters = /[A-Z]/g;
-    if (myInput.match(upperCaseLetters)) {
-        $("#capital").removeClass("invalid");
-        $("#capital").addClass("valid");
-    } else {
-        $("#capital").removeClass("valid");
-        $("#capital").addClass("invalid");
-    }
+ $.fn.addrows = function(formData){
+    const tablebody = $("#myTable tbody");
+    const newRow = $("<tr></tr>");
 
-    // Validate numbers
-    var numbers = /[0-9]/g;
-    if (myInput.match(numbers)) {
-        $("#number").removeClass("invalid");
-        $("#number").addClass("valid");
-    } else {
-        $("#number").removeClass("valid");
-        $("#number").addClass("invalid");
-    }
+    const emailCell = $("<td></td>");
+    const phoneCell = $("<td></td>");
+    const firstnameCell = $("<td></td>");
+    const lastnameCell = $("<td></td>");
 
-    // Validate length
-    if (myInput.length >= 8) {
-        $("#length").removeClass("invalid");
-        $("#length").addClass("valid");
-    } else {
-        $("#length").removeClass("valid");
-        $("#length").addClass("invalid");
-    }
+    emailCell.text(formData.email);
+    phoneCell.text(formData.phone);
+    firstnameCell.text(formData.firstname);
+    lastnameCell.text(formData.lastname);
 
-})
+    newRow.append(emailCell);
+    newRow.append(phoneCell);
+    newRow.append(firstnameCell);
+    newRow.append(lastnameCell);
+
+    //create edit button for each newly added row
+    const editbutton =$("<button></button>");
+    editbutton.text("Edit");
+    editbutton.addClass("button-edit");
+    editbutton.click(function(){
+        $.fn.handleEdit(formData);
+    })
+
+    const deletebutton=$("<button></button>");
+    deletebutton.text("Delete");
+    deletebutton.addClass("button-delete");
+    deletebutton.click(function(){
+        $.fn.handleDelete(formData);
+    })
+
+    const commonbutton=$("<td></td>");
+    commonbutton.addClass("buttons");
+    commonbutton.append(editbutton, deletebutton);
+
+    newRow.append(commonbutton);
+    tablebody.append(newRow);
+
+    $("#myForm")[0].reset();
+ }
+
+ 
+ $.fn.handleEdit = function (formData) {
+    $.each(formData, function (key, value) {
+        let temp="#" + key;
+        $(temp).val(formData[key]);
+        if (formData.relocation=="yes") {
+            $("#yes").prop("checked", true);
+        }
+        else{
+            $("#no").prop("checked", true);
+        }
+
+    })
+    $("#buttondetail").text("Update");
+    editrow=formData;
+ }
+ $.fn.handleDelete = function (formData) {
+  
+     
+ }
+
+    
+
+ 
+
+
 
 
 
